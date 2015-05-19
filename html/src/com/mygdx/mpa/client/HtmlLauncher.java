863f47a5a3b5e06e141f91aa8bd11a8a5b637088
Karol Daniel Skorupski <daniel.karol.skorupski@gmail.com>
2015-05-19 19:31:06 +0200

    Create project
    Libgdx Project Generator - Clean

---
+++ html/src/com/mygdx/mpa/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.mpa.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.mpa.MPA;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MPA();
+        }
+}
